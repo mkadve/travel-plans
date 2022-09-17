b0814bff54eb2f25532ee104360554b39435bf67
codal-mkadve <mkadve@codal.com>
2022-09-18 02:25:08 +0530

    added debounce time

--- client/src/app/travel-plans-list/travel-plans.component.ts
+++ client/src/app/travel-plans-list/travel-plans.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { TravelPlansService } from '../travel-plans.service';
 import * as mapboxgl from 'mapbox-gl';
 
@@ -14,17 +15,32 @@ export class TravelPlansComponent implements OnInit {
   private style = 'mapbox://styles/mapbox/streets-v11';
   private lat = 37.75;
   private lng = -122.41;
+  public cityName: string = '';
+  subject = new Subject()
 
   travelPlans$: Observable<any[]> = new Observable();
+  results$: Observable<any> = new Observable();
   travelPlansByCity$: Observable<any[]> = new Observable();
   timeout: any = null;
   cityLists: any = [];  
   citySearch: string = '';
   travelData: any;
+  private readonly searchSubject = new Subject<string | undefined>();
+  private searchSubscription?: Subscription;
+
 
   constructor(private travelPlansService: TravelPlansService) { }
 
   ngOnInit(): void {
+    this.searchSubscription = this.searchSubject
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((searchQuery:any) => this.travelPlansService.getPlanById(searchQuery))
+    )
+    .subscribe((results) => {
+      this.cityLists = results['cities']
+    });
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
@@ -33,32 +49,20 @@ export class TravelPlansComponent implements OnInit {
   });
   // Add map controls
   this.map.addControl(new mapboxgl.NavigationControl());
+    
   }
 
   cityClick(city: any) {
+    this.cityName = city.name;
     this.travelPlansByCity$ = this.travelPlansService.getPlanByCity(city);
     this.travelPlansByCity$.subscribe((res : any)=>{
-      console.log(res);
       this.travelData = res.data;
+      this.cityLists = [];
     })
   }
 
   onKeySearch(event: any) {
     this.citySearch = event.target.value
-    clearTimeout(this.timeout);
-    var $this = this;
-    this.timeout = setTimeout(function () {
-      if (event.keyCode != 13) {
-        $this.getPlanById(event.target.value);
-      }
-    }, 1000);
-  }
-
-  private getPlanById(value: any): void {
-    this.travelPlans$ = this.travelPlansService.getPlanById(value);
-    this.travelPlans$.subscribe((res : any)=>{
-      this.cityLists = res['cities'];
-      
-    })
+    this.searchSubject.next(this.citySearch.trim());
   }
 }
